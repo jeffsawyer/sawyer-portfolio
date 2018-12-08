59fb9186ad6a606eb4366b90e6c7c5fc3e44a672
Jeff Sawyer <jeff.sawyer@gmail.com>
2018-12-07 22:56:04 -0800

    cleaned up dependencies and imports after getting ajax working

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+/*import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
+*/
 interface Article {
 	title: string;
 	slug: string;
